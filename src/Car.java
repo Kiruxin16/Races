c09bed0a16bf8a275c6286ff6e38e786212c070e
Kiruxin16 <94542550+Kiruxin16@users.noreply.github.com>
2022-04-04 10:13:38 +0700

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,9 @@
-public class Car implements Runnable {
+public class Car implements Runnable {		
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
+	private CyclicBarrier cycb;
     private Race race;
     private int speed;
     private String name;
@@ -12,7 +13,8 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(CyclicBarrier cycb, Race race, int speed) {
+	this.cycb =cycb;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
@@ -24,6 +26,7 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+		cycb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
